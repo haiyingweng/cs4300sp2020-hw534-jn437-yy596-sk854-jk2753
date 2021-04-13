0d0b7ecddc0550e8637bc5159c9d83a6cfaff20c
Haiying Weng <hw534@cornell.edu>
2021-04-12 23:58:20 -0400

    Add cereal reviews script and some reviews

---
+++ cereal_reviews.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from os import environ
+
+REVIEWS_URL = "https://target-com-store-product-reviews-locations-data.p.rapidapi.com/product/reviews"
+
+
+def get_reviews(tcin):
+    query_string = {"tcin": tcin, "limit": "100", "offset": "0"}
+    headers = {
+        "x-rapidapi-key": environ["RAPIDAPI_KEY"],
+        "x-rapidapi-host": environ["RAPIDAPI_HOST"],
+    }
+    response = requests.request(
+        "GET", REVIEWS_URL, headers=headers, params=query_string
+    )
+    response_json = response.json()
+    if tcin not in reviews and tcin not in inserted_data:
+        inserted_data[tcin] = response_json
+
+
+product_tcins = ["12935649"]
+inserted_data = {}
+
+with open("reviews.json") as f:
+    reviews = json.load(f)
+
+for tcin in product_tcins:
+    get_reviews(tcin)
+
+reviews.update(inserted_data)
+
+with open("reviews.json", "w") as f:
+    json.dump(reviews, f)
