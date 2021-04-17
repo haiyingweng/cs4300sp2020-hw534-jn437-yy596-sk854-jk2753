39603a523d2bcb4957aa4bca38bff9e1d342cbf9
Sooah Kang <sk854@cornell.edu>
2021-04-17 13:32:40 -0400

    search results

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,21 +1,22 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-from .calculate_similarity import inverted_index, idf, norms, rank_by_similarity, get_cereal_details
-
+from .calculate_similarity import *
 project_name = "Cerealizer"
 net_id = "Joie Ng: jn437, Ying Yang: yy596, Haiying Weng: hw534, Jason Jungwoo Kim: jk2753, Sooah Kang: sk854"
 
-@irsystem.route('/search=?', methods=['GET'])
+@irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	if not query:
 		data = []
 		output_message = ''
+		print(data)
 	else:
 		output_message = "Your search: " + query
 		ranked_cereals = rank_by_similarity(query, inverted_index, idf, norms)
 		data = get_cereal_details(ranked_cereals)
+		print(data)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
